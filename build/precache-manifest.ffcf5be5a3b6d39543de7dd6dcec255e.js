1004e40b8aa774fb3deeb93722234a05df8c6d36
yangchch6 <ycc54140127@163.com>
2020-07-15 17:08:58 +0800

    update

---
+++ build/precache-manifest.ffcf5be5a3b6d39543de7dd6dcec255e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cee0b2f328105aab680d92c649cd2e7e",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "000fae1f59432bfbf954",
+    "url": "/github-actions-demo/static/css/main.692d75bb.chunk.css"
+  },
+  {
+    "revision": "5d5c73a8bdbb4b3155dc",
+    "url": "/github-actions-demo/static/js/2.10039b62.chunk.js"
+  },
+  {
+    "revision": "000fae1f59432bfbf954",
+    "url": "/github-actions-demo/static/js/main.a2fb5043.chunk.js"
+  },
+  {
+    "revision": "41870333dfc813cf444c",
+    "url": "/github-actions-demo/static/js/runtime~main.dd1e6690.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-actions-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
